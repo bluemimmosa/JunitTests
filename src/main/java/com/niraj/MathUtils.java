24e478b069765da6ae2deb2e6af841938c6ac6a3
bluemimmosa <console1913@gmail.com>
2023-09-30 09:00:35 +0545

    Initial Commit. Learned about junit5 and its jupiter and vintage api along with the platform called the junit engine. Wrote a basic test class and learned how to use test in java.

---
+++ src/main/java/com/niraj/MathUtils.java
@@ -0,0 +1,12 @@
+package com.niraj;
+
+public class MathUtils {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int div(int a, int b){
+        return a/b;
+    }
+}
---
+++ src/test/java/com/niraj/MathUtilsTest.java
@@ -0,0 +1,13 @@
+package com.niraj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilsTest {
+
+    @Test
+    void test(){
+        System.out.println("This test ran!");
+    }
+}
